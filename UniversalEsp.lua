a9afaab62a1d7f0f3a19ea91fb807ab268ab944e
zzerexx <74355984+zzerexx@users.noreply.github.com>
2021-06-13 16:11:47 -0700

    Update UniversalEsp.lua

--- UniversalEsp.lua
+++ UniversalEsp.lua
@@ -9,9 +9,12 @@ if typeof(Drawing.new) ~= "function" then
 end
 if typeof(getgenv().UNIVERSALESP_OBJECTS) == "table" then
     for i,v in next, getgenv().UNIVERSALESP_OBJECTS do
-        v:Remove()
+        v.Object:Remove()
         table.remove(getgenv().UNIVERSALESP_OBJECTS,i)
     end
+end
+if typeof(getgenv().UNIVERSALESP_RS) == "RBXScriptConnection" then
+    getgenv().UNIVERSALESP_RS:Disconnect()
 end
 
 local players = game:GetService("Players")
@@ -21,115 +24,31 @@ local mouse = game:GetService("UserInputService"):GetMouseLocation()
 local RunService = game:GetService("RunService")
 local ss = getgenv().EspSettings
 getgenv().UNIVERSALESP_OBJECTS = {}
+getgenv().UNIVERSALESP_VISIBLE = true
 
 function Box(plr)
+    ss = getgenv().EspSettings
     local box = Drawing.new("Square")
     box.Visible = true
     box.Transparency = ss.Boxes.Transparency
     box.Color = ss.Boxes.Color
     box.Thickness = 1
     box.Filled = false
-    local rs = RunService.RenderStepped:Connect(function()
-        ss = getgenv().EspSettings
-        if ss.Boxes.Enabled and plr and plr ~= player and plr.Character and plr.Character:FindFirstChild("Head") and plr.Character:FindFirstChild("HumanoidRootPart") and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
-            local vector, inViewport = camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
-            
-            local root = plr.Character.HumanoidRootPart
-            local head = plr.Character.Head
-            local rootpos = camera:WorldToViewportPoint(root.Position)
-            local headpos = camera:WorldToViewportPoint(head.Position + Vector3.new(0,0.5,0))
-            local legpos = camera:WorldToViewportPoint(root.Position - Vector3.new(0,3,0))
-
-            if inViewport then
-                box.Transparency = ss.Boxes.Transparency
-                box.Size = Vector2.new(1000 / rootpos.Z, headpos.Y - legpos.Y)
-                box.Position = Vector2.new(rootpos.X - box.Size.X / 2, rootpos.Y - (box.Size.Y / 2))
-
-                if ss.TeamCheck and plr.Team == player.Team then
-                    box.Visible = false
-                else
-                    box.Visible = true
-                end
-            else
-                box.Visible = false
-            end
-            if ss.Boxes.UseTeamColor then
-                box.Color = plr.TeamColor.Color
-            else
-                box.Color = ss.Boxes.Color
-            end
-        else
-            box.Visible = false
-        end
-    end)
-    players.PlayerRemoving:Connect(function(p)
-        if p == plr then
-            box:Remove()
-            rs:Disconnect()
-        end
-    end)
-    table.insert(getgenv().UNIVERSALESP_OBJECTS,#getgenv().UNIVERSALESP_OBJECTS+1,box)
+    table.insert(getgenv().UNIVERSALESP_OBJECTS,{Object = box,Type = "Box",Player = plr})
 end
 
 function Tracer(plr)
+    ss = getgenv().EspSettings
     local tracer = Drawing.new("Line")
     tracer.Visible = true
     tracer.Transparency = ss.Tracers.Transparency
     tracer.Color = ss.Tracers.Color
     tracer.Thickness = ss.Tracers.Thickness
-    local rs = RunService.RenderStepped:Connect(function()
-        ss = getgenv().EspSettings
-        if ss.Tracers.Enabled and plr and plr ~= player and plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
-            local vector, inViewport = camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
-            
-            local top = Vector2.new(camera.ViewportSize.X / 2, 0)
-            local center = Vector2.new(camera.ViewportSize.X / 2,camera.ViewportSize.Y / 2)
-            local bottom = Vector2.new(camera.ViewportSize.X / 2,camera.ViewportSize.Y)
-            mouse = game:GetService("UserInputService"):GetMouseLocation()
-            local origin = top
-            if ss.Tracers.Origin:lower() == "top" then
-                origin = top
-            elseif ss.Tracers.Origin:lower() == "center" then
-                origin = center
-            elseif ss.Tracers.Origin:lower() == "bottom" then
-                origin = bottom
-            elseif ss.Tracers.Origin:lower() == "mouse" then
-                origin = mouse
-            end
-
-            if inViewport then
-                tracer.Transparency = ss.Tracers.Transparency
-                tracer.Thickness = ss.Tracers.Thickness
-                tracer.From = origin
-                tracer.To = Vector2.new(vector.X,vector.Y)
-
-                if ss.TeamCheck and plr.Team == player.Team then
-                    tracer.Visible = false
-                else
-                    tracer.Visible = true
-                end
-            else
-                tracer.Visible = false
-            end
-            if ss.Tracers.UseTeamColor then
-                tracer.Color = plr.TeamColor.Color
-            else
-                tracer.Color = ss.Tracers.Color
-            end
-        else
-            tracer.Visible = false
-        end
-    end)
-    players.PlayerRemoving:Connect(function(p)
-        if p == plr then
-            tracer:Remove()
-            rs:Disconnect()
-        end
-    end)
-    table.insert(getgenv().UNIVERSALESP_OBJECTS,#getgenv().UNIVERSALESP_OBJECTS+1,tracer)
+    table.insert(getgenv().UNIVERSALESP_OBJECTS,{Object = tracer,Type = "Tracer",Player = plr})
 end
 
 function Name(plr)
+    ss = getgenv().EspSettings
     local name = Drawing.new("Text")
     name.Visible = true
     name.Transparency = ss.Names.Transparency
@@ -140,78 +59,156 @@ function Name(plr)
     name.Outline = ss.Names.Outline
     name.OutlineColor = ss.Names.OutlineColor
     name.Font = ss.Names.Font
-    local rs = RunService.RenderStepped:Connect(function()
+    table.insert(getgenv().UNIVERSALESP_OBJECTS,{Object = name,Type = "Name",Player = plr})
+end
+
+getgenv().UNIVERSALESP_RS = RunService.RenderStepped:Connect(function()
+    for i,v in next, getgenv().UNIVERSALESP_OBJECTS do
         ss = getgenv().EspSettings
-        if ss.Names.Enabled and plr and plr ~= player and plr.Character and plr.Character:FindFirstChild("Head") and plr.Character:FindFirstChild("HumanoidRootPart") and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
-            local vector, inViewport = camera:WorldToViewportPoint(plr.Character.Head.Position)
-            if inViewport then
-                name.Transparency = ss.Names.Transparency
-                name.Size = ss.Names.Size
-                name.Outline = ss.Names.Outline
-                name.OutlineColor = ss.Names.OutlineColor
-                name.Position = Vector2.new(vector.X,vector.Y-30)
-                name.Font = ss.Names.Font
-                if ss.TeamCheck and plr.Team == player.Team then
+        local plr = v.Player
+        if v.Type == "Box" then
+            local box = v.Object
+            if getgenv().UNIVERSALESP_VISIBLE and ss.Boxes.Enabled and plr and plr ~= player and plr.Character and plr.Character:FindFirstChild("Head") and plr.Character:FindFirstChild("HumanoidRootPart") and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
+                local vector, inViewport = camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
+                
+                local root = plr.Character.HumanoidRootPart
+                local head = plr.Character.Head
+                local rootpos = camera:WorldToViewportPoint(root.Position)
+                local headpos = camera:WorldToViewportPoint(head.Position + Vector3.new(0,0.5,0))
+                local legpos = camera:WorldToViewportPoint(root.Position - Vector3.new(0,3,0))
+
+                if inViewport then
+                    box.Transparency = ss.Boxes.Transparency
+                    box.Size = Vector2.new(1000 / rootpos.Z, headpos.Y - legpos.Y)
+                    box.Position = Vector2.new(rootpos.X - box.Size.X / 2, rootpos.Y - (box.Size.Y / 2))
+
+                    if ss.TeamCheck and plr.Team == player.Team then
+                        box.Visible = false
+                    else
+                        box.Visible = true
+                    end
+                else
+                    box.Visible = false
+                end
+                if ss.Boxes.UseTeamColor then
+                    box.Color = plr.TeamColor.Color
+                else
+                    box.Color = ss.Boxes.Color
+                end
+            else
+                box.Visible = false
+            end
+        elseif v.Type == "Tracer" then
+            local tracer = v.Object
+            if getgenv().UNIVERSALESP_VISIBLE and ss.Tracers.Enabled and plr and plr ~= player and plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
+                local vector, inViewport = camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
+                
+                local top = Vector2.new(camera.ViewportSize.X / 2, 0)
+                local center = Vector2.new(camera.ViewportSize.X / 2,camera.ViewportSize.Y / 2)
+                local bottom = Vector2.new(camera.ViewportSize.X / 2,camera.ViewportSize.Y)
+                mouse = game:GetService("UserInputService"):GetMouseLocation()
+                local origin = top
+                if ss.Tracers.Origin:lower() == "top" then
+                    origin = top
+                elseif ss.Tracers.Origin:lower() == "center" then
+                    origin = center
+                elseif ss.Tracers.Origin:lower() == "bottom" then
+                    origin = bottom
+                elseif ss.Tracers.Origin:lower() == "mouse" then
+                    origin = mouse
+                end
+    
+                if inViewport then
+                    tracer.Transparency = ss.Tracers.Transparency
+                    tracer.Thickness = ss.Tracers.Thickness
+                    tracer.From = origin
+                    tracer.To = Vector2.new(vector.X,vector.Y)
+    
+                    if ss.TeamCheck and plr.Team == player.Team then
+                        tracer.Visible = false
+                    else
+                        tracer.Visible = true
+                    end
+                else
+                    tracer.Visible = false
+                end
+                if ss.Tracers.UseTeamColor then
+                    tracer.Color = plr.TeamColor.Color
+                else
+                    tracer.Color = ss.Tracers.Color
+                end
+            else
+                tracer.Visible = false
+            end
+        elseif v.Type == "Name" then
+            local name = v.Object
+            if getgenv().UNIVERSALESP_VISIBLE and ss.Names.Enabled and plr and plr ~= player and plr.Character and plr.Character:FindFirstChild("Head") and plr.Character:FindFirstChild("HumanoidRootPart") and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
+                local vector, inViewport = camera:WorldToViewportPoint(plr.Character.Head.Position)
+                if inViewport then
+                    name.Transparency = ss.Names.Transparency
+                    name.Size = ss.Names.Size
+                    name.Outline = ss.Names.Outline
+                    name.OutlineColor = ss.Names.OutlineColor
+                    name.Position = Vector2.new(vector.X,vector.Y-30)
+                    name.Font = ss.Names.Font
+                    if ss.TeamCheck and plr.Team == player.Team then
+                        name.Visible = false
+                    else
+                        name.Visible = true
+                    end
+                    local mag = 0
+                    if player.Character:FindFirstChild("HumanoidRootPart") then
+                        mag = math.floor((player.Character.HumanoidRootPart.Position - plr.Character.HumanoidRootPart.Position).Magnitude)
+                    end
+                    local health = math.floor(plr.Character:FindFirstChildOfClass("Humanoid").Health)
+                    local maxhealth = math.floor(plr.Character:FindFirstChildOfClass("Humanoid").MaxHealth)
+                    local plrname = plr.Name
+                    if ss.Names.UseDisplayName then
+                        plrname = plr.DisplayName
+                    end
+                    if ss.Names.ShowDistance and ss.Names.ShowHealth then
+                        name.Text = plrname.." [ "..mag.." ] [ "..health.."/"..maxhealth.." ]"
+                    elseif ss.Names.ShowDistance then
+                        name.Text = plrname.." [ "..mag.." ]"
+                    elseif ss.Names.ShowHealth then
+                        name.Text = plrname.." [ "..health.."/"..maxhealth.." ]"
+                    else
+                        name.Text = plrname
+                    end
+                else
                     name.Visible = false
-                else
-                    name.Visible = true
                 end
-                local mag = (player.Character.HumanoidRootPart.Position - plr.Character.HumanoidRootPart.Position).Magnitude
-                local health = plr.Character:FindFirstChildOfClass("Humanoid").Health
-                local maxhealth = plr.Character:FindFirstChildOfClass("Humanoid").MaxHealth
-                local plrname = plr.Name
-                if ss.Names.UseDisplayName then
-                    plrname = plr.DisplayName
-                end
-                if ss.Names.ShowDistance and ss.Names.ShowHealth then
-                    name.Text = plrname.." [ "..math.floor(mag).." ] [ "..math.floor(health).."/"..math.floor(maxhealth).." ]"
-                elseif ss.Names.ShowDistance then
-                    name.Text = plrname.." [ "..math.floor(mag).." ]"
-                elseif ss.Names.ShowHealth then
-                    name.Text = plrname.." [ "..math.floor(health).."/"..math.floor(maxhealth).." ]"
+                if ss.Names.UseTeamColor then
+                    name.Color = plr.TeamColor.Color
                 else
-                    name.Text = plrname
+                    name.Color = ss.Names.Color
                 end
             else
                 name.Visible = false
             end
-            if ss.Names.UseTeamColor then
-                name.Color = plr.TeamColor.Color
-            else
-                name.Color = ss.Names.Color
-            end
-        else
-            name.Visible = false
         end
-    end)
-    players.PlayerRemoving:Connect(function(p)
-        if p == plr then
-            name:Remove()
-            rs:Disconnect()
-        end
-    end)
-    table.insert(getgenv().UNIVERSALESP_OBJECTS,#getgenv().UNIVERSALESP_OBJECTS+1,name)
-end
-
+    end
+end)
+game:GetService("UserInputService").InputBegan:Connect(function(i,gp)
+    if not gp and i.KeyCode == ss.ToggleKey then
+        getgenv().UNIVERSALESP_VISIBLE = not getgenv().UNIVERSALESP_VISIBLE
+    end
+end)
 for i,v in next, players:GetPlayers() do
-    if ss.Boxes.Enabled then
-        Box(v)
-    end
-    if ss.Tracers.Enabled then
-        Tracer(v)
-    end
-    if ss.Names.Enabled then
-        Name(v)
-    end
+    Box(v)
+    Tracer(v)
+    Name(v)
 end
 players.PlayerAdded:Connect(function(v)
-    if ss.Boxes.Enabled then
-        Box(v)
-    end
-    if ss.Tracers.Enabled then
-        Tracer(v)
-    end
-    if ss.Names.Enabled then
-        Name(v)
+    Box(v)
+    Tracer(v)
+    Name(v)
+end)
+players.PlayerRemoving:Connect(function(plr)
+    for i,v in next, getgenv().UNIVERSALESP_OBJECTS do
+        if v.Player == plr then
+            v.Object:Remove()
+            table.remove(getgenv().UNIVERSALESP_OBJECTS,i)
+        end
     end
 end)
