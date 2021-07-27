896e12aaa7af0248e7da43fced0f4eb0b9ae31df
zzerexx <74355984+zzerexx@users.noreply.github.com>
2021-07-27 14:49:39 -0700

    Update UniversalAimbot.lua

--- UniversalAimbot.lua
+++ UniversalAimbot.lua
@@ -1,18 +1,32 @@
-getgenv().AimbotSettings = {
-    Aimbot = {
-        TargetPart = "Head",
-        VisibleCheck = true,
-    },
-    Fov = {
-        Enabled = true,
-        Dynamic = true,
-        Radius = 100,
-        Transparency = 1,
-        Color = Color3.fromRGB(255,255,255),
-        NumSides = 64,
-    },
-    Whitelisted = {} -- Username or User ID
-}
+if not getgenv().AimbotSettings then
+    getgenv().AimbotSettings = {
+        Aimbot = {
+            TargetPart = "Head",
+            VisibleCheck = true,
+            Use_mousemoverel = true,
+            TeamCheck = true, -- Press ] to toggle
+        },
+        FovCircle = {
+            Enabled = true,
+            Dynamic = true,
+            Radius = 100,
+            Transparency = 1,
+            Color = Color3.fromRGB(255,255,255),
+            NumSides = 64,
+        },
+        Whitelisted = {}, -- Username or User ID
+        Ignore = nil -- Raycast Ignore
+    }
+end
+
+if typeof(getgenv().AIMBOT_FOV) == ("userdata" or "table") then
+    getgenv().AIMBOT_FOV:Remove()
+    getgenv().AIMBOT_FOV = nil
+end
+if typeof(getgenv().AIMBOT_RS) == "RBXScriptConnection" then
+    getgenv().AIMBOT_RS:Disconnect()
+    getgenv().AIMBOT_RS = nil
+end
 
 local players = game:GetService("Players")
 local player = players.LocalPlayer
@@ -39,7 +53,7 @@ function ClosestPlayer()
             local vector, inViewport = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
             if inViewport then
                 local mag = (Vector2.new(mouse.X,mouse.Y) - Vector2.new(vector.X,vector.Y)).Magnitude
-                if mag < closest and ((player.Team ~= nil and v.Team ~= player.Team) or player.Team == nil) then
+                if mag < closest and ((player.Team ~= nil and v.Team ~= player.Team) or (ss.TeamCheck or player.Team == nil)) then
                     plr = v
                     closest = mag
                 end
@@ -56,6 +70,9 @@ function IsVisible(plr)
                 params.FilterDescendantsInstances = {camera,player.Character}
                 params.FilterType = Enum.RaycastFilterType.Blacklist
                 params.IgnoreWater = true
+                if ss.Ignore ~= nil and typeof(ss.Ignore) == "Instance" then
+                    params.FilterDescendantsInstances = {camera,player.Character,ss.Ignore}
+                end
                 local result = workspace:Raycast(camera.CFrame.Position,(v.Position - camera.CFrame.Position).Unit * 500,params)
                 if result and result.Instance:FindFirstAncestor(plr.Name) then
                     return true
@@ -71,17 +88,17 @@ local fov
 function InFov(plr)
     mouse = UIS:GetMouseLocation()
     if IsAlive(plr) then
-        local vector, inViewport = camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
-        if ss.Fov.Enabled and inViewport then
+        local _, inViewport = camera:WorldToViewportPoint(plr.Character[ss.Aimbot.TargetPart].Position)
+        if ss.FovCircle.Enabled and inViewport then
             for i,v in next, plr.Character:GetChildren() do
                 if table.find(bodyparts,v.Name) then
                     local vector2, inViewport2 = camera:WorldToViewportPoint(v.Position)
-                    if inViewport2 and (Vector2.new(mouse.X,mouse.Y) - Vector2.new(vector2.X,vector2.Y)).Magnitude <= (fov.Radius or ss.Fov.Radius) then
+                    if inViewport2 and (Vector2.new(mouse.X,mouse.Y) - Vector2.new(vector2.X,vector2.Y)).Magnitude <= (fov.Radius or ss.FovCircle.Radius) then
                         return true
                     end
                 end
             end
-        elseif not ss.Fov.Enabled and inViewport then
+        elseif not ss.FovCircle.Enabled and IsAlive(plr) then
             return true
         end
     end
@@ -100,6 +117,8 @@ UIS.InputBegan:Connect(function(i,gp)
     end
     if i.UserInputType == Enum.UserInputType.MouseButton2 or i.KeyCode == Enum.KeyCode.Q then
         ads = true
+    elseif i.KeyCode == Enum.KeyCode.RightBracket then
+        ss.TeamCheck = not ss.TeamCheck
     end
 end)
 UIS.InputEnded:Connect(function(i,gp)
@@ -121,14 +140,16 @@ fov.Radius = 100
 fov.Filled = false
 getgenv().AIMBOT_FOV = fov
 getgenv().AIMBOT_RS = RunService.RenderStepped:Connect(function()
-    if ss.Fov.Enabled then
-        fov.Position = UIS:GetMouseLocation()
-        fov.NumSides = ss.Fov.NumSides
-        fov.Radius = ss.Fov.Radius
-        fov.Transparency = ss.Fov.Transparency
-        fov.Color = ss.Fov.Color
-        if ss.Fov.Dynamic then
-            fov.Radius = ss.Fov.Radius / (camera.FieldOfView / 100)
+    ss = getgenv().AimbotSettings
+    mouse = UIS:GetMouseLocation()
+    if ss.FovCircle.Enabled then
+        fov.Position = mouse
+        fov.NumSides = ss.FovCircle.NumSides
+        fov.Radius = ss.FovCircle.Radius
+        fov.Transparency = ss.FovCircle.Transparency
+        fov.Color = ss.FovCircle.Color
+        if ss.FovCircle.Dynamic then
+            fov.Radius = ss.FovCircle.Radius / (camera.FieldOfView / 100)
         end
     else
         fov.Transparency = 0
@@ -136,8 +157,13 @@ getgenv().AIMBOT_RS = RunService.RenderStepped:Connect(function()
     if ads then
         local plr = ClosestPlayer()
         if plr ~= nil then
-            if (ss.Aimbot.VisibleCheck and IsVisible(plr)) and InFov(plr) then
-                camera.CFrame = CFrame.new(camera.CFrame.Position,plr.Character[ss.Aimbot.TargetPart].Position)
+            if IsVisible(plr) and InFov(plr) and not IsWhitelisted(plr) and plr.Character:FindFirstChild(ss.Aimbot.TargetPart) then
+                if ss.Aimbot.Use_mousemoverel then
+                    local vector = camera:WorldToViewportPoint(plr.Character[ss.Aimbot.TargetPart].Position)
+                    mousemoverel(vector.X - mouse.X,vector.Y - mouse.Y)
+                else
+                    camera.CFrame = CFrame.new(camera.CFrame.Position,plr.Character[ss.Aimbot.TargetPart].Position)
+                end
             end
         end
     end
