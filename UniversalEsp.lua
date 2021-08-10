a8798a5bfe0fa1663a10504b18a5c24341a8fac1
zzerexx <74355984+zzerexx@users.noreply.github.com>
2021-08-10 15:54:57 -0700

    Update UniversalEsp.lua

--- UniversalEsp.lua
+++ UniversalEsp.lua
@@ -500,13 +500,13 @@ end)
 local esp = {}
 
 function ValidType(type)
-	if getgenv().EspSettings[type] ~= nil then
+	if type == "Other" or getgenv().EspSettings[type] ~= nil then
 		return true
 	end
 	return false
 end
 function ValidOption(type,option)
-	if getgenv().EspSettings[type][option] ~= nil then
+	if type == "Other" or getgenv().EspSettings[type][option] ~= nil then
 		return true
 	end
 	return false
