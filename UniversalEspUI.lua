bed7686d84cc66c098e05c4849a683ea08e9dee0
zzerexx <74355984+zzerexx@users.noreply.github.com>
2021-12-28 15:10:06 -0800

    Update UniversalEspUI.lua

--- UniversalEspUI.lua
+++ UniversalEspUI.lua
@@ -1,57 +1,52 @@
 getgenv().EspSettings = {
-	TeamCheck = false,
-	ToggleKey = Enum.KeyCode.RightAlt,
-	AntiDetection = true,
-	Boxes = {
-		Enabled = true,
-		Transparency = 0.7,
-		Color = Color3.fromRGB(255,255,255),
-		UseTeamColor = true,
-	},
-	Tracers = {
-		Enabled = true,
-		Transparency = 0.7,
-		Color = Color3.fromRGB(255,255,255),
-		UseTeamColor = true,
-		Origin = "Top", -- "Top" or "Center" or "Bottom" or "Mouse"
-		Thickness = 1
-	},
-	Names = {
-		Enabled = true,
-		Transparency = 0.7,
-		Color = Color3.fromRGB(255,255,255),
-		UseTeamColor = true,
-		Font = Drawing.Fonts.UI, -- UI or System or Plex or Monospace
-		Size = 18,
-		Outline = true,
-		OutlineColor = Color3.fromRGB(0,0,0),
-		ShowDistance = false,
-		ShowHealth = false,
-		UseDisplayName = false,
-	},
-	Skeletons = {
-		Enabled = true,
-		Transparency = 0.7,
-		Color = Color3.fromRGB(255,255,255),
-		UseTeamColor = true,
-		Thickness = 1
-	},
-	LookTracers = {
-		Enabled = true,
-		Transparency = 0.7,
-		Color = Color3.fromRGB(255,255,255),
-		UseTeamColor = true,
-		IgnoreWater = true,
-		Thickness = 1
-	},
-	HealthBars = {
-		Enabled = true,
-		Transparency = 0.7,
-		Color = Color3.fromRGB(0,255,0),
-		OutlineColor = Color3.fromRGB(255,255,255),
-		UseTeamColor = true -- this only applies to the outline
-	}
-} -- v1.5.2
+    TeamCheck = false,
+    ToggleKey = Enum.KeyCode.RightAlt,
+    AntiDetection = true,
+    RefreshRate = 10, -- how fast the esp updates (milliseconds)
+    Boxes = {
+        Enabled = true,
+        Transparency = 1,
+        Color = Color3.fromRGB(255,255,255),
+        UseTeamColor = true,
+    },
+    Tracers = {
+        Enabled = true,
+        Transparency = 1,
+        Color = Color3.fromRGB(255,255,255),
+        UseTeamColor = true,
+        Origin = "Top", -- "Top" or "Center" or "Bottom" or "Mouse"
+        Thickness = 1
+    },
+    Names = {
+        Enabled = true,
+        Transparency = 1,
+        Color = Color3.fromRGB(255,255,255),
+        UseTeamColor = true,
+        Font = Drawing.Fonts.UI, -- UI or System or Plex or Monospace
+        Size = 18,
+        Outline = true,
+        OutlineColor = Color3.fromRGB(0,0,0),
+        ShowDistance = false,
+        ShowHealth = true,
+        UseDisplayName = false,
+        DistanceDataType = "m", -- what it says after the distance (ex. 100m)
+        HealthDataType = "Percentage" -- "Percentage" or "Value"
+    },
+    Skeletons = {
+        Enabled = true,
+        Transparency = 1,
+        Color = Color3.fromRGB(255,255,255),
+        UseTeamColor = true,
+        Thickness = 1
+    },
+    HealthBars = {
+        Enabled = true,
+        Transparency = 1,
+        Color = Color3.fromRGB(0,255,0),
+        OutlineColor = Color3.fromRGB(255,255,255),
+        UseTeamColor = true -- this only applies to the outline
+    }
+} -- v1.6.0
 
 if UESP then
 	return
