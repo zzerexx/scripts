1a3d02c8bc4c6396e85791b73aa268fdc0da032e
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-01-02 14:18:13 -0800

    Update UniversalAimbotUI.lua

--- UniversalAimbotUI.lua
+++ UniversalAimbotUI.lua
@@ -1,29 +1,27 @@
 getgenv().AimbotSettings = {
+	TeamCheck = true, -- Press ] to toggle
+	VisibleCheck = true,
+	RefreshRate = 10, -- how fast the aimbot updates (milliseconds)
+	Keybind = Enum.UserInputType.MouseButton2, -- Must be a UserInputType or KeyCode
+	ToggleKey = Enum.KeyCode.RightShift,
+	MaximumDistance = 300, -- Set this to something lower if you dont wanna lock on some random person across the map
+	AlwaysActive = false,
 	Aimbot = {
 		Enabled = true,
 		TargetPart = "Head",
-		VisibleCheck = true,
 		Use_mousemoverel = true,
 		Strength = 100, -- 1% - 200%
-		TeamCheck = true, -- Press ] to toggle
-		Keybind = Enum.UserInputType.MouseButton2, -- Must be a UserInputType or KeyCode
 		AimType = "Hold", -- "Hold" or "Toggle"
-		AlwaysActive = false,
-		ToggleKey = Enum.KeyCode.RightShift,
-		MaximumDistance = 300, -- Set this to something lower if you dont wanna lock on some random person across the map
 	},
 	AimAssist = {
 		Enabled = false,
-		ToggleKey = Enum.KeyCode.RightShift,
 		MinFov = 15,
 		MaxFov = 80,
 		DynamicFov = true,
 		ShowFov = false, -- Shows Min & Max fov
 		Strength = 55, -- 1% - 100%
-		AlwaysActive = true,
 		SlowSensitivity = true,
 		SlowFactor = 1.75, -- 1% - 10%
-		MaximumDistance = 300,
 		RequireMovement = true,
 	},
 	FovCircle = {
@@ -37,7 +35,7 @@ getgenv().AimbotSettings = {
 	Whitelisted = {}, -- Username or User ID
 	WhitelistFriends = true, -- Automatically adds friends to the whitelist
 	Ignore = nil -- Raycast Ignore
-} -- v1.1.10
+} -- v1.1.11
 
 if UAIMBOT then
 	return
@@ -46,7 +44,6 @@ if OldInstance then
 	getgenv().OldInstance = nil
 end
 
-local HttpService = game:GetService("HttpService")
 local players = game:GetService("Players")
 local aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/UniversalAimbot.lua"))()
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
@@ -63,8 +60,10 @@ end
 
 local Aimbot = UI.New({Title = "Aimbot"})
 local Assist = UI.New({Title = "Aim Assist"})
+local Settings = UI.New({Title = "Settings"})
 local Fov = UI.New({Title = "Fov Circle"})
 local Other = UI.New({Title = "Other"})
+local ui = OldInstance
 
 Aimbot.Toggle({
 	Text = "Enabled",
@@ -73,19 +72,17 @@ Aimbot.Toggle({
 	end,
 	Enabled = true
 })
-Aimbot.TextField({
-	Text = "Target Part (Head is default)",
-	Type = "Default",
+Aimbot.Dropdown({
+	Text = "Target Part",
 	Callback = function(value)
 		aimbot:Set("Aimbot","TargetPart",value)
-	end
-})
-Aimbot.Toggle({
-	Text = "Visible Check",
-	Callback = function(value)
-		aimbot:Set("Aimbot","VisibleCheck",value)
 	end,
-	Enabled = true
+	Options = {"Head","UpperTorso","Torso","HumanoidRootPart"},
+	Menu = {
+		Info = function()
+			Banner("Some games don't have certain parts. For example, Arsenal doesn't have Torso, so you will have to select UpperTorso.")
+		end
+	}
 })
 Aimbot.Toggle({
 	Text = "Use mousemoverel",
@@ -103,42 +100,12 @@ Aimbot.Slider({
 	Max = 200,
 	Def = 100
 })
-Aimbot.Toggle({
-	Text = "Team Check",
-	Callback = function(value)
-		aimbot:Set("Aimbot","TeamCheck",value)
-	end,
-	Enabled = true
-})
 Aimbot.Dropdown({
 	Text = "Aim Type",
 	Callback = function(value)
 		aimbot:Set("Aimbot","AimType",value)
 	end,
 	Options = {"Hold","Toggle"}
-})
-Aimbot.Toggle({
-	Text = "Always Active",
-	Callback = function(value)
-		aimbot:Set("Aimbot","AlwaysActive",value)
-	end,
-	Enabled = false
-})
-Aimbot.TextField({
-	Text = "Toggle Key (RightShift is default)",
-	Type = "Default",
-	Callback = function(value)
-		aimbot:Set("Aimbot","ToggleKey",Enum.KeyCode[value])
-	end
-})
-Aimbot.Slider({
-	Text = "Maximum Distance",
-	Callback = function(value)
-		aimbot:Set("Aimbot","MaximumDistance",value)
-	end,
-	Min = 0,
-	Max = 1000,
-	Def = 300,
 })
 
 Assist.Toggle({
@@ -154,7 +121,7 @@ Assist.Slider({
 		aimbot:Set("AimAssist","MinFov",value)
 	end,
 	Min = 0,
-	Max = 30,
+	Max = 50,
 	Def = 15
 })
 Assist.Slider({
@@ -190,20 +157,6 @@ Assist.Slider({
 	Def = 55
 })
 Assist.Toggle({
-	Text = "AlwaysActive",
-	Callback = function(value)
-		aimbot:Set("AimAssist","AlwaysActive",value)
-	end,
-	Enabled = true
-})
-Assist.TextField({
-	Text = "Toggle Key (RightShift is default)",
-	Type = "Default",
-	Callback = function(value)
-		aimbot:Set("AimAssist","ToggleKey",Enum.KeyCode[value])
-	end
-})
-Assist.Toggle({
 	Text = "Slow Sensitivity",
 	Callback = function(value)
 		aimbot:Set("AimAssist","SlowSensitivity",value)
@@ -219,25 +172,77 @@ Assist.Slider({
 	Max = 100,
 	Def = 17
 })
-Assist.Slider({
-	Text = "Maximum Distance",
-	Callback = function(value)
-		if value == 0 then
-			aimbot:Set("AimAssist","MaximumDistance",math.huge)
-		else
-			aimbot:Set("AimAssist","MaximumDistance",value)
-		end
-	end,
-	Min = 0,
-	Max = 1000,
-	Def = 300
-})
 Assist.Toggle({
 	Text = "Require Movement",
 	Callback = function(value)
 		aimbot:Set("AimAssist","RequireMovement",value)
 	end,
 	Enabled = true
+})
+
+Settings.Toggle({
+	Text = "Team Check",
+	Callback = function(value)
+		aimbot:Set("Other","TeamCheck",value)
+	end,
+	Enabled = true
+})
+Settings.Toggle({
+	Text = "Visible Check",
+	Callback = function(value)
+		aimbot:Set("Other","VisibleCheck",value)
+	end,
+	Enabled = true
+})
+Settings.Slider({
+	Text = "Refresh Rate",
+	Callback = function(value)
+		aimbot:Set("Other","RefreshRate",value)
+	end,
+	Min = 0,
+	Max = 50,
+	Def = 10
+})
+Settings.TextField({
+	Text = "Toggle Key",
+	Type = "Default",
+	Callback = function(value)
+		if Enum.KeyCode[value] then
+			aimbot:Set("Other","Keybind",Enum.KeyCode[value])
+		else
+			Banner("Invalid KeyCode.\nVisit https://developer.roblox.com/en-us/api-reference/enum/KeyCode for a list of KeyCodes.")
+		end
+	end,
+	Menu = {
+		Info = function()
+			Banner("This is the keybind that toggles aimbot and aim assist (switches between the two). Default is RightShift. Must be a valid KeyCode!")
+		end,
+		KeyCodes = function()
+			setclipboard("https://developer.roblox.com/en-us/api-reference/enum/KeyCode")
+			Banner("Copied to your clipboard!")
+		end
+	}
+})
+Settings.Slider({
+	Text = "Maximum Distance",
+	Callback = function(value)
+		aimbot:Set("Other","MaximumDistance",value)
+	end,
+	Min = 0,
+	Max = 2000,
+	Def = 300
+})
+Settings.Toggle({
+	Text = "Always Active",
+	Callback = function(value)
+		aimbot:Set("Other","AlwaysActive",value)
+	end,
+	Enabled = false,
+	Menu = {
+		Info = function()
+			Banner("If enabled, aimbot/aim assist will always be active, even without holding right click.")
+		end
+	}
 })
 
 Fov.Toggle({
@@ -295,11 +300,10 @@ Other.TextField({
 	Text = "Username or User ID",
 	Type = "Default",
 	Callback = function(value)
-		if value:gsub(" ","") ~= "" and not players:FindFirstChild(value) then
-			return Banner(value.." is not in the game.")
+		if not players:FindFirstChild(value) then
+			return Banner(value.." is not in the game. (must be a username or user id, not display name)")
 		end
 		plr = value
-		print(plr)
 	end
 })
 Other.Button({
@@ -331,11 +335,33 @@ Other.Toggle({
 	Callback = function(value)
 		aimbot:Set("Other","WhitelistFriends",value)
 	end,
-	Enabled = true
+	Enabled = true,
+	Menu = {
+		Info = function()
+			Banner("If enabled, your friends will automatically be whitelisted upon joining.")
+		end
+	}
 })
 Other.Button({
 	Text = "Destroy Aimbot",
 	Callback = function()
 		aimbot:Destroy()
-	end
+		ui:Destroy()
+	end,
+	Menu = {
+		Info = function()
+			Banner("This button will completely remove Universal Aimbot, including the UI.")
+		end
+	}
+})
+Other.Button({
+	Text = "Credits",
+	Callback = function()
+		Banner("Material UI Library created by twink marie\nUniversal Aimbot created by zzerexx#3970")
+	end,
+	Menu = {
+		Version = function()
+			Banner("v1.1.11")
+		end
+	}
 })
