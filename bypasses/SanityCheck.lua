03d77bfc1740228823f4b0d69a2e7bdaff1037eb
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-02-12 03:28:48 -0800

    Update SanityCheck.lua

--- bypasses/SanityCheck.lua
+++ bypasses/SanityCheck.lua
@@ -9,3 +9,15 @@ local old;old = hookmetamethod(game, "__namecall", function(...)
 	
 	return old(...)
 end)
+
+
+--[[
+	info
+	LocalPlayer.PlayerScripts.SLOCK -- decompiled: https://pastebin.com/raw/8c3Z66PV
+	LocalPlayer.PlayerScripts.Sprint -- decompiled: https://pastebin.com/raw/eS1t7CsD
+	LocalPlayer.PlayerGui.MessageText.LocalScript -- decompiled: https://pastebin.com/raw/LcAXGehN
+
+	ReplicatedStorage.RemoteEvents.ClientKick -- just kicks the player (lol?)
+	ReplicatedStorage.RemoteEvents.RemoveCharacter -- removes the character (lol??)
+	ReplicatedStorage.RemoteEvents.removecserver -- removes the character but on the server (lol???)
+]]
