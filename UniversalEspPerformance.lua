6faad64c5dc4c58fd9062668f9f5fe3a1595487f
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-02-14 20:18:45 -0800

    Update UniversalEspPerformance.lua

--- UniversalEspPerformance.lua
+++ UniversalEspPerformance.lua
@@ -78,71 +78,115 @@ elseif GameId == gids.bb then
 		end
 	end
 end
-
 local oldfuncs = {}
+
 function IsAlive(plr)
 	local humanoid = FindFirstChild(plr.Character or game, "Humanoid")
-	if plr.Character and humanoid and humanoid.Health > 0 then
+	if humanoid and humanoid.Health > 0 then
 		return true
-	elseif getchar then
-		return getchar(plr) ~= nil
-	elseif ts then
-		return characters:GetCharacter(plr) ~= nil
 	end
 	return false
 end
-oldfuncs.alive = IsAlive
-function GetChar(plr)
+do
 	if getchar then
-		local a = getchar(plr)
-		if a ~= nil then
-			return a.torso.Parent
+		IsAlive = function(plr)
+			return getchar(plr) ~= nil
 		end
-	elseif ts then
-		return characters:GetCharacter(plr)
-	elseif plr.Character ~= nil then
-		return plr.Character
 	end
-	return nil
+	if ts then
+		IsAlive = function(plr)
+			return characters:GetCharacter(plr) ~= nil
+		end
+	end
+end
+oldfuncs.alive = IsAlive
+
+function GetChar(plr)
+	return plr.Character
+end
+do
+	if getchar then
+		GetChar = function(plr)
+			local a = getchar(plr)
+			if a ~= nil then
+				return a.torso.Parent
+			end
+			return nil
+		end
+	end
+	if ts then
+		GetChar = function(plr)
+			return characters:GetCharacter(plr)
+		end
+	end
 end
 oldfuncs.character = GetChar
+
 function GetHealth(plr)
-	local a = plr.Character
-	local humanoid = FindFirstChild(a or game, "Humanoid")
-	if gethealth then
-		return {mathfloor(gethealth(plr,plr)),100}
-	elseif ts then 
-		a = characters:GetCharacter(plr)
-		if FindFirstChild(a, "Health") then
-			return {mathfloor(a.Health.Value), mathfloor(a.Health.MaxHealth.Value)}
-		end
-	elseif GameId == gids.arsenal then
-		a = plr.NRPBS
-		return {mathfloor(a.Health.Value), mathfloor(a.MaxHealth.Value)}
-	elseif a ~= nil and humanoid then
-		return {mathfloor(humanoid.Health), mathfloor(humanoid.MaxHealth)}
+	local a = FindFirstChild(plr.Character or game, "Humanoid")
+	if a then
+		return {mathfloor(a.Health), mathfloor(a.MaxHealth)}
 	end
 	return {100,100}
 end
-oldfuncs.health = GetHealth
-function GetTeam(plr)
-	if ts then
-		return teams:GetPlayerTeam(plr)
+do
+	if gethealth then
+		GetHealth = function(plr)
+			return {mathfloor(gethealth(plr, plr)), 100}
+		end
 	end
+	if ts then
+		GetHealth = function(plr)
+			local a = characters:GetCharacter(plr)
+			local hp = FindFirstChild(a, "Health")
+			if hp then
+				return {mathfloor(hp.Value), mathfloor(hp.MaxHealth.Value)}
+			end
+			return {100,100}
+		end
+	end
+	if GameId == gids.arsenal then
+		GetHealth = function(plr)
+			local a = plr.NRPBS
+			return {mathfloor(a.Health.Value), mathfloor(a.MaxHealth.Value)}
+		end
+	end
+end
+oldfuncs.health = GetHealth
+
+function GetTeam(plr)
 	return plr.Team
 end
+do
+	if ts then
+		GetTeam = function(plr)
+			return teams:GetPlayerTeam(plr)
+		end
+	end
+end
 oldfuncs.team = GetTeam
+
 function IsFFA()
 	local t = {}
 	for _,v in next, players:GetPlayers() do
-		if v.Team == nil then return true end
-		local team = v.Team.Name
+		local team = GetTeam(v)
+		if team == nil then return true end
+		team = team.Name or team
 		if not t[team] then
 			tableinsert(t, team)
 		end
 	end
 	return #t == 1
 end
+do
+	if GameId == gids.arsenal then
+		IsFFa = function()
+			return game:GetService("ReplicatedStorage").wkspc.FFA.Value == true
+		end
+	end
+end
+oldfuncs.ffa = IsFFA
+
 function ApplyZIndex(obj,name)
 	if ZIndexEnabled then
 		local idx = zindex[name]
@@ -211,10 +255,11 @@ end
 
 local lastupdate = osclock()
 function update()
-	if osclock() - lastupdate < 0.01 then
+	if osclock() - lastupdate < 0.01 then -- 10ms
 		return
 	end
 	lastupdate = osclock()
+	local ffa, myteam, ccf = IsFFA(), GetTeam(player), camera.CFrame.Position
 	for _,v in next, OBJECTS do
 		if not v.Destroyed then
 			if v.Player == nil then
@@ -224,13 +269,13 @@ function update()
 			local plr, obj, type = v.Player, v.Object, v.Type					-- objects shit
 			local cf, size, mid, inViewport, tl, tr, bl, br					  -- boxes shit
 			local tlx, tly, trx, try, blx, bly, brx, bry						 -- boxes corner axes shit
-			local team, myteam, teamcolor										-- team shit
-			local ffa, ccf, char, health, maxhealth, mag, mousemag, render	   -- other shit
+			local team, teamcolor												-- team shit
+			local char, health, maxhealth, mag, mousemag, render				 -- other shit
 			if VISIBLE and plr and IsAlive(plr) then
 				local hp = GetHealth(plr)
-				ffa, ccf, char, health, maxhealth = IsFFA(), camera.CFrame.Position, GetChar(plr), hp[1], hp[2]
+				char, health, maxhealth = GetChar(plr), hp[1], hp[2]
 				cf, size = char:GetBoundingBox()
-				team, myteam, teamcolor = GetTeam(plr), GetTeam(player), plr.TeamColor.Color
+				team, teamcolor = GetTeam(plr), plr.TeamColor.Color
 				mag = (ccf - cf.Position).Magnitude
 				render = ffa or (not ffa and team ~= myteam) and mag <= 300
 				if render then
@@ -255,7 +300,7 @@ function update()
 					SetProp(obj, "Visible", render)
 					if inViewport and render then
 						local color = (ts and team == "Omega" and omega or team == "Beta" and beta) or teamcolor
-						local transparency = (mousemag <= 75 and 0.25) or 1
+						local transparency = (mousemag <= 75 and 0.35) or 1
 						
 						SetProp(obj, "Transparency", transparency)
 						SetProp(obj, "Color", color)
@@ -332,8 +377,9 @@ function esp:SetFunction(a,f)
 		GetHealth = f
 	elseif a == "team" then
 		GetTeam = f
+	elseif a == "ffa" then
+		IsFFA = f
 	end
-	
 end
 function esp:ResetFunction(a)
 	assert(typeof(a) == "string",("Universal Esp: bad argument to #1 'SetFunction' (string expected, got %s)"):format(typeof(a)))
@@ -348,6 +394,8 @@ function esp:ResetFunction(a)
 		GetHealth = f
 	elseif a == "team" then
 		GetTeam = f
+	elseif a == "ffa" then
+		IsFFA = f
 	end
 end
 function esp:Destroy()
