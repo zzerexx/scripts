82953249f412758174e2920681dd9457a57897f5
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-02-26 19:00:22 -0800

    Update secure_call.lua

--- SynapseFunctions/secure_call.lua
+++ SynapseFunctions/secure_call.lua
@@ -1,9 +1,14 @@
--- bypasses getfenv detections
--- DOES NOT bypass debug.traceback detection
+-- idk if its fully secure but it returns the same results as synapse
+local old;old = hookfunction(getrenv().debug.traceback,function(lol)
+	if checkcaller() then
+		return lol.."\n"..old():split("\n")[2].."\n"
+	end
+	return old()
+end)
 return function(func,env,...)
 	return coroutine.wrap(function(...)
-		setfenv(0, getsenv(env))
-		setfenv(1, getsenv(env))
+		setfenv(0,getsenv(env))
+		setfenv(1,getsenv(env))
 		return func(...)
 	end)(...)
 end
