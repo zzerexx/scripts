5b610464ac8e10dc2e82b1a96c306021cecac3d3
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-06-25 22:59:08 -0700

    Create Aimblox.lua

---
+++ Games/Aimblox.lua
@@ -0,0 +1,9 @@
+--[[
+
+            -- WARNING --
+    THIS SCRIPT IS VERY OUTDATED
+       USE AT YOUR OWN CAUTION
+
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/Aimblox.lua"))()
