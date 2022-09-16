81c50cd900c94892a8f9b990205174d403212714
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-09-15 20:23:47 -0700

    Update secure_call.lua

--- SynapseFunctions/secure_call.lua
+++ SynapseFunctions/secure_call.lua
@@ -1,14 +1,38 @@
--- idk if its fully secure but it returns the same results as synapse
-local old;old = hookfunction(getrenv().debug.traceback,function(lol)
+-- security is not guaranteed
+local oldt;oldt = hookfunction(getrenv().debug.traceback, function(lol)
+	local traceback = oldt(lol)
 	if checkcaller() then
-		return lol.."\n"..old():split("\n")[2].."\n"
+		local a = traceback:split("\n")
+		return string.format("%s\n%s\n", a[1], a[3])
 	end
-	return old()
+	return traceback
 end)
-return function(func,env,...)
+local oldi;oldi = hookfunction(getrenv().debug.info, function(lvl, a)
+	if checkcaller() then
+		return oldi(3, a)
+	end
+	return oldi(lvl, a)
+end)
+
+local setidentity = setidentity or setthreadidentity or set_thread_identity or setthreadcontext or set_thread_context or (syn and syn.set_thread_identity)
+local function secure_call(func, env, ...)
+	local functype = typeof(func) 
+	local envtype = typeof(env)
+	assert(functype == "function", string.format("bad argument to #1 'secure_call' (function expected, got %s)", functype))
+	assert(envtype == "Instance", string.format("bad argument to #2 'secure_call' (Instance expected, got %s)", envtype))
+	local envclass = env.ClassName
+	assert(envclass == "LocalScript" or envclass == "ModuleScript", string.format("bad argument to #2 'secure_call' (LocalScript or ModuleScript expected, got %s)", envclass))
+	local _, fenv = xpcall(function()
+		return getsenv(env)
+	end, function()
+		return getfenv(func)
+	end)
 	return coroutine.wrap(function(...)
-		setfenv(0,getsenv(env))
-		setfenv(1,getsenv(env))
+		setidentity(2)
+		setfenv(0, fenv)
+		setfenv(1, fenv)
 		return func(...)
 	end)(...)
 end
+
+return secure_call
