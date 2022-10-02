5a4073bab2f77f1aaf8b022d47d75813db36c588
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-10-02 00:26:50 -0700

    Update Orca.lua

--- Popular/Orca.lua
+++ Popular/Orca.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua", "Orca"))()
