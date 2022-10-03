4ad211ff5302d6cad30718e6bf0298e3a13a274f
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-10-02 17:13:06 -0700

    Create Hydroxide.lua

---
+++ Popular/Hydroxide.lua
@@ -0,0 +1,9 @@
+local owner = "Upbolt"
+local branch = "revision"
+
+local function webImport(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+end
+
+webImport("init")
+webImport("ui/main")
