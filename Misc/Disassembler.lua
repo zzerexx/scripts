e6db94bf495fefff367cdaf242d2171b9e920b77
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-11-26 19:44:58 -0800

    Update Disassembler.lua

--- Misc/Disassembler.lua
+++ Misc/Disassembler.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSeaweedMonster/Luau/main/decompile.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSeaweedMonster/Luau/main/decompile.lua"), "Disassembler")()
